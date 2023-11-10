d9086ff32acef0a4b68e76c93f58688d8a47af26
oleksandr-saulko <aleksandrsaulko@gmail.com>
2023-11-10 21:24:04 +0200

    add javascript-with-functions solution

---
+++ freecodecamp/javascript-algorithms/basic-javaScript/write-reusable-javascript-with-functions.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION 
+// 1. Create a function called reusableFunction which prints the string Hi World to the dev console.
+// 2. Call the function.
+
+// ✅ SOLUTION:
+
+function reusableFunction() {
+  console.log("Hi World");
+}
+
+reusableFunction();
