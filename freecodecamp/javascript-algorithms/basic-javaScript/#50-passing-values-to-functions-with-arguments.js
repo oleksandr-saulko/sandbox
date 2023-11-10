39ebeec8b06334aeacce189b6c337e38bfb19ca6
oleksandr-saulko <aleksandrsaulko@gmail.com>
2023-11-10 21:37:03 +0200

    passing-values-to-functions-with-arguments sol

---
+++ freecodecamp/javascript-algorithms/basic-javaScript/#50-passing-values-to-functions-with-arguments.js
@@ -0,0 +1,11 @@
+// ❓ DESCRIPTION
+// 1. Create a function called functionWithArgs that accepts two arguments and outputs their sum to the dev console.
+// 2. Call the function with two numbers as arguments.
+
+// ✅ SOLUTION:
+
+function functionWithArgs(firstNumber, secondNumber) {
+  console.log(firstNumber + secondNumber);
+}
+
+functionWithArgs(27, 43);
