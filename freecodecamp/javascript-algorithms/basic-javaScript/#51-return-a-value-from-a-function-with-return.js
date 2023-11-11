40cd6981502e7d9bcd2270e47bbe75fb9ac1c50d
oleksandr-saulko <aleksandrsaulko@gmail.com>
2023-11-11 08:28:15 +0200

    return-a-value-from-a-function solution

---
+++ freecodecamp/javascript-algorithms/basic-javaScript/#51-return-a-value-from-a-function-with-return.js
@@ -0,0 +1,8 @@
+// ❓ DESCRIPTION
+// Create a function timesFive that accepts one argument, multiplies it by 5, and returns the new value.
+
+// ✅ SOLUTION:
+
+function timesFive(num) {
+  return num * 5;
+}
