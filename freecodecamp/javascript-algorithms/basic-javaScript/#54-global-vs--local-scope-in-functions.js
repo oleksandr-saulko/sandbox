986c63df8d01b5e205b31e9fe0687e57c7073bfb
oleksandr-saulko <aleksandrsaulko@gmail.com>
2023-11-21 12:14:33 +0200

    global-vs--local-scope-in-functions solution

---
+++ freecodecamp/javascript-algorithms/basic-javaScript/#54-global-vs--local-scope-in-functions.js
@@ -0,0 +1,16 @@
+// ❓DESCRIPTION:
+// Add a local variable to myOutfit function to override the value of outerWear with the string sweater.
+
+// ✅SOLUTION:
+
+// Setup
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+  const outerWear = 'sweater';
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
